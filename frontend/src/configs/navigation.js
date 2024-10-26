f205a5caa8e704519e4b6a6cd94271f0d9761a57
Danil <deltayt.ro@gmail.com>
2024-10-26 15:56:31 +0300

    frontend 1.0.0.2

--- frontend/src/configs/navigation.js
+++ frontend/src/configs/navigation.js
@@ -1,23 +1,7 @@
 export default [
   {
     title: 'Проекты',
-    href: '/recipes',
-    auth: false
-  },{
-    title: 'Мои подписки',
-    href: '/subscriptions',
-    auth: true
-  },{
-    title: 'Создать проект',
-    href: '/recipes/create',
-    auth: true
-  },{
-    title: 'Избранное',
-    href: '/favorites',
-    auth: true
-  }, {
-    title: 'Список проектов',
-    href: '/cart',
+    href: '/tasks',
     auth: true
   }
 ]
